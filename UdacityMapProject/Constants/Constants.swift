8df58bc5321db1678529b7dbc9d43dd9fb99798c
Kynan <k.song@blueyonder.co.uk>
2018-11-16 12:37:33 +0000

    Created project

---
+++ UdacityMapProject/Constants/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  UdacityMapProject
+//
+//  Created by Kynan Song on 16/11/2018.
+//  Copyright © 2018 Kynan Song. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    
+    static let parseID = "QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr"
+    static let restAPIKey = "QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY"
+}
